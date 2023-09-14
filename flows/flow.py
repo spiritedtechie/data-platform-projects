52480799df23644f652bf37b6e5c3001f358e207
Vijay Patel <spiritedtechie@gmail.com>
2023-09-14 13:15:10 +0100

    feat: add additional soda data quality tests to orchestration flow

--- flows/flow.py
+++ flows/flow.py
@@ -15,11 +15,12 @@ postgres = Postgres.load("default")
 @task
 def load_raw_data_to_store():
     postgres.load_csv("./datasets/countries.csv", schema="raw")
-    return postgres.load_csv("./datasets/invoices.csv", schema="raw")
+    postgres.load_csv("./datasets/invoices.csv", schema="raw")
+    return None
 
 
-@task
-def quality_check_raw_data():
+@task(task_run_name="quality_check_{name}")
+def quality_check(name: str, soda_check: SodaCLCheck, data_source: str = "raw"):
     flow_run_ctx = FlowRunContext.get()
     flow_run_name = flow_run_ctx.flow_run.name
     scan_results_file_path = f"{flow_run_name}.json"
@@ -28,10 +29,9 @@ def quality_check_raw_data():
     soda_configuration = SodaConfiguration(
         configuration_yaml_path="./soda/configuration.yml"
     )
-    soda_check = SodaCLCheck(sodacl_yaml_path="./soda/checks/sources/raw_invoices.yml")
 
     return soda_scan_execute.fn(
-        data_source_name="raw",
+        data_source_name=data_source,
         configuration=soda_configuration,
         checks=soda_check,
         scan_results_file=scan_results_file_path,
@@ -43,7 +43,7 @@ def quality_check_raw_data():
 
 
 @task
-def create_models():
+def build_models():
     env = {**postgres.as_env_properties()}
     dbt_init = DbtCoreOperation(
         commands=["dbt debug", "dbt run"],
@@ -52,16 +52,35 @@ def create_models():
         overwrite_profiles=False,
         env=env,
     )
-    dbt_init.run()
+    return dbt_init.run()
 
 
 @flow(name="Retail data", log_prints=True)
 def process_retail_data():
     load = load_raw_data_to_store()
 
-    check = quality_check_raw_data(wait_for=[load])
+    raw_data_check = quality_check(
+        name="source_data",
+        wait_for=[load],
+        data_source="raw",
+        soda_check=SodaCLCheck(sodacl_yaml_path="./soda/checks/sources/*.yml"),
+    )
 
-    create_models(wait_for=[check])
+    create_models = build_models(wait_for=[raw_data_check])
+
+    staged_data_check = quality_check(
+        name="staged_data",
+        wait_for=[create_models],
+        data_source="retail",
+        soda_check=SodaCLCheck(sodacl_yaml_path="./soda/checks/staged/*.yml"),
+    )
+
+    transform_data_check = quality_check(
+        name="transform_data",
+        wait_for=[create_models],
+        data_source="retail",
+        soda_check=SodaCLCheck(sodacl_yaml_path="./soda/checks/transform/*.yml"),
+    )
 
 
 if __name__ == "__main__":
