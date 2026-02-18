506fa02d3dc42795572b57bb55b84d9504c46fcf
Vijay Patel <spiritedtechie>
2026-02-18 17:11:48 +0000

    Removing old v1 platform artefacts - as no longer needed nor that useful, and relevant parts will be picked out and rebuilt

--- dbt/online_retail/models/metrics/top_10_customer_revenue.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  c.id as customer_id,
-  SUM(fi.total) AS total_revenue
-FROM {{ ref('fct_invoices') }} fi
-JOIN {{ ref('dim_customer') }} c ON fi.customer_id = c.id
-GROUP BY c.id
-ORDER BY total_revenue DESC
-LIMIT 10
