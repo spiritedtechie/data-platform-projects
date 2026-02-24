4d879b20518e32150ef8d2d839f5e510d8a6226e
Vijay Patel <spiritedtechie>
2026-02-24 17:24:07 +0000

    v2- corrects status mapping logic, since severity 20 means line closed and should not be in the same bucket as 'good service'

--- version_2/dbt_gold/macros/status_mapping.sql
+++ version_2/dbt_gold/macros/status_mapping.sql
@@ -1,26 +1,28 @@
 {% macro status_category_from_severity(status_severity_col) -%}
 case
-    when {{ status_severity_col }} >= 10 then 'Good Service'
+    when {{ status_severity_col }} = 10 then 'Good Service'
     when {{ status_severity_col }} = 9 then 'Minor Delays'
     when {{ status_severity_col }} = 8 then 'Severe Delays'
-    when {{ status_severity_col }} in (7, 6, 5) then 'Suspended / Closed'
+    when {{ status_severity_col }} = 20 then 'Closed'
+    when {{ status_severity_col }} in (7, 6, 5) then 'Suspended'
     else 'Other'
 end
 {%- endmacro %}
 
 {% macro severity_weight_from_severity(status_severity_col) -%}
 case
-    when {{ status_severity_col }} >= 10 then 0.0
+    when {{ status_severity_col }} = 10 then 0.0
     when {{ status_severity_col }} = 9 then 1.0
     when {{ status_severity_col }} = 8 then 2.0
     when {{ status_severity_col }} = 7 then 3.0
     when {{ status_severity_col }} in (6, 5) then 4.0
+    when {{ status_severity_col }} = 20 then 5.0
     else 2.0
 end
 {%- endmacro %}
 
 {% macro is_good_service_from_severity(status_severity_col) -%}
-case when {{ status_severity_col }} >= 10 then true else false end
+case when {{ status_severity_col }} = 10 then true else false end
 {%- endmacro %}
 
 {% macro recovery_flag_from_severity(prev_status_severity_col, new_status_severity_col) -%}
